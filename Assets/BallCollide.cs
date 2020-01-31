18bf954af0c6f0448e1bb40e59537a645be14fc9
Tan Jun Ming <jtan@csumb.edu>
2020-01-30 22:49:37 -0800

    Add movement controls and physics

---
+++ Assets/BallCollide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollide : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            rb.velocity = new Vector3(5, 0, 3);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            rb.velocity = new Vector3(-5, 0, -3);
+        }
+    }
+}
---
+++ Assets/PaddleMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMove : MonoBehaviour
+{
+    public Transform paddle;
+    public int player;
+
+    string horizontal;
+    string vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        horizontal = string.Format("P{0}_Horizontal", player);
+        vertical = string.Format("P{0}_Vertical", player);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vec = new Vector3();
+        int speed = 5;
+
+        vec.x = Input.GetAxis(horizontal) * Time.deltaTime * speed;
+        vec.z = Input.GetAxis(vertical) * Time.deltaTime * speed;
+
+        paddle.position += vec;
+
+    }
+}
